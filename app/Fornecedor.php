63b4240d1ddb485cbf6c15f5c6c192269b8e531b
carloserig <carlos.erig@gmail.com>
2022-03-17 23:50:29 -0300

    migrations criadas

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    //
+    //objeto criado como fornecedors
+    protected $table = 'fornecedores';
+    //autorizar criar registro com create 
+    protected $fillable = ['nome','site','uf','email'];
 }
